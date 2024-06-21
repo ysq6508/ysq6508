227423058011a6681a4d0d591683c18e69d62209
yanshuqing6508 <2357078540@qq.com>
2024-06-21 21:36:55 +0800

    components

---
+++ vue/components/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="app">
+        <button class="btn" @click="changeTab(1)">购物车</button>
+        <button class="btn" @click="changeTab(2)">todos</button>
+
+
+        <!--shopping-->
+        <Shopping v-if="tabIndex === 1"></Shopping>
+
+        <!--todos-->
+        <Todos v-else></Todos>
+    </div>
+  </template>
+  
+  <script setup>
+    import { ref } from 'vue'
+    import Shopping from './components/Shopping.vue'  // 引入组件
+    import Todos from './components/Todos.vue'
+
+    let tabIndex = ref(1)
+
+    const changeTab = (index) => {
+        console.log(index);
+        tabIndex.value = index
+    }
+
+  </script>
+  
+  <style lang="css" scoped>
+    .app {
+        text-align: center;
+    }
+    .btn {
+        font-size: 20px;
+        width: 100px;
+        height: 40px;
+        margin: 20px;
+    }
+  </style>
---
+++ vue/components/src/components/Todos.vue
@@ -0,0 +1,47 @@
+<template>
+    <section class="todoapp">
+        <header class="header">
+            <h1>todos</h1>
+            <input type="text" class="new-todo" placeholder="想做的事" @keyup.enter="addTodo">
+        </header>
+
+        <section class="main">
+            <input type="checkbox" class="toggle-all">
+            <label for="toggle-all">Mark all as complete</label>
+
+            <ul class="todo-list">
+                <li class="todo" v-for="todo in state.todos">
+                    <div class="view">
+                        <input type="checkbox" class="toggle" v-model="todo.completed">
+                        <label>{{todo.title}}</label>
+                        <button class="destroy"></button>
+                    </div>
+                </li>
+            </ul>
+        </section>
+
+    </section>    
+</template>
+  
+  <script setup>
+  import { reactive } from 'vue'
+
+  const state = reactive({
+    todos: [
+        {id: 1, title: '吃饭', completed: false},
+        {id: 2, title: '睡觉', completed: true},
+        {id: 3, title: '打豆豆', completed: false}
+    ]
+  })
+
+  // 1. 输入new todo 增加列表
+  // 2. 手动完成某件事项
+  // 3. 手动移除某事项
+  // 4. 全选 || 全不选
+    
+  </script>
+  
+  <style lang="css" scoped>
+  
+  </style>
+  
