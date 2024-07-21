977cfedcbae822f68fb373bd231db8e877a1fc5f
yanshuqing6508 <2357078540@qq.com>
2024-07-21 23:09:34 +0800

    vue-todos

---
+++ vue/todos/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <ul>
+        <li v-for="(item, index) in arr" key="index">{{ item }}</li>
+      </ul>
+
+      <button @click="handle">push</button>
+      <button @click="reverse">reverse</button>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+
+let arr = reactive(['html', 'css', 'js', 'vue'])
+
+const handle = () => {
+  arr.push('node')
+}
+
+const reverse = () => {
+  // arr.reverse()
+  /*arr = arr.map((item) => {  // map 会返回一个新数组，但新数组不是响应式，此法无用
+    return item + '1';
+  })*/
+  arr.forEach((item, index) => {  // map 会返回一个新数组，但新数组不是响应式，此法无用
+    arr[index] = item + '1';
+  })
+}
+
+</script>
+
+<style lang="css" scoped>
+
+</style>
---
+++ vue/todos/src/App2.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div>
+            账号：<input type="text" v-model="state.username">
+        </div>
+        <div>
+            密码：<input type="text" v-model="state.password">
+        </div>
+        <button @click="login">登录</button>
+    </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+const state = reactive({
+    username: '',
+    password: ''
+})
+const login = () => {
+    state.username = 'admin'
+    console.log(state);
+}
+
+</script>
+
+<style lang="css" scoped>
+
+</style>
