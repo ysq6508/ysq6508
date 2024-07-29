9ed787a77502aff7ae133dd2f86a7ac03dded328
yanshuqing6508 <2357078540@qq.com>
2024-07-30 00:01:28 +0800

    react-transforms

---
+++ react/react-transforms/README.md
@@ -0,0 +1,29 @@
+- 介绍自己
+
+- react 怎么聊？
+    - vue搞得多，react 也玩了下， mvvm，组件化，响应式，迁移下很快的 一周左右把
+        三天，试下看
+    - vue 优势是API设计优秀，对新手友好，文档全
+        - react JS 风格激进，函数式编程，JSX，hooks在业内遥遥领先
+            对新手不太友好
+        - vue3 从vue2 选项式API发展到setup 模式，在函数式，hooks 这块也支持的很好了
+        - vue3 现在对ts 的支持的很好了
+        - vue react 都可以用，看团队和项目的需要
+    - react 响应式 useState 天生的hooks 函数式基因
+
+前端实习生/AI全栈实习生
+- 以node来到这家公司，做一些AI的结构调用
+
+- 你怎么看待前端和AI发展
+    - 前端因AI 用户体验改变了，前端的天职是用户体验，所以要学AI
+        - ChatGPT 就是全新的产品体验形式，robot助理
+            openai 推出了 chat search，挑战google搜索
+        - AI 生成图片并上传 AIGC coze
+        - 熟悉 openai 的接口调用
+        - 通义千问等code copilot 提升开发效率，干预实战学习并实战大前端，全栈等技能，
+            未来技术鸿沟会越来越小，自己已经准备好利用AIGC 的工具
+        - transformer.js langchain.js AI框架已经拥抱前端
+
+- transformer.js
+    来自全球最大的AI社区 HugginFace，全球的LLM都在这里发布
+    transformer.js 是一个NLP 机器学习库，语言翻译
---
+++ react/react-transforms/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import LanguageSelector from './components/LanguageSelector';
+// react 组件 = 函数 + JSX 
+// JSX ? 
+function App() {
+  // vue ref 
+  const [sourceLanguage, setSourceLanguage] = useState('zho_Hans');
+  const [targetLanguage, setTargetLanguage] = useState('eng_Latn');
+  const [input, setInput] = useState('')
+  const [output, setOutput] = useState('')
+    // ref reactive Model 
+    // useState hooks 思想直接内置到框架
+  const headerEle = (
+    <h1>首页</h1>
+  )
+  return (
+    // template  view 
+    <>
+      <h1>Transformer.js</h1>
+      <h2>ML-powered multiligual translation in React!</h2>
+      <div className="container">
+        <div className="language-container">
+          <LanguageSelector 
+            type={"原语言"} 
+            defaultLanguage={sourceLanguage}
+            onChange={x => setSourceLanguage(x.target.value)}   
+          />
+          <LanguageSelector 
+            type={"目标语言"} 
+            defaultLanguage={targetLanguage}
+            onChange={x => setTargetLanguage(x.target.value)}   
+          />
+        </div>
+        <div className="textbox-container">
+          <textarea 
+            value={input} 
+            rows={3} 
+            onChange={e => setInput(e.target.value)}
+          />
+          <textarea 
+            value={output} 
+            rows={3} 
+            readOnly/>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
