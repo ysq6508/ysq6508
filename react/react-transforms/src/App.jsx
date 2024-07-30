6f2a6cf61fe10da65b9ef9a37ecb785dca4ff3dd
yanshuqing6508 <2357078540@qq.com>
2024-07-30 12:05:15 +0800

    react-transformer

--- react/react-transforms/src/App.jsx
+++ react/react-transforms/src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import LanguageSelector from './components/LanguageSelector';
+import './App.css'
 // react 组件 = 函数 + JSX 
 // JSX ? 
 function App() {
@@ -8,6 +9,23 @@ function App() {
   const [targetLanguage, setTargetLanguage] = useState('eng_Latn');
   const [input, setInput] = useState('')
   const [output, setOutput] = useState('')
+  const [disabled, setDisabled] = useState(false)
+
+  useEffect(() => {
+    // vue onMounted 
+    console.log('life cycle')
+    // 耗时性任务交给worker 子线程
+    // JS 有多线程机制，html5 web worker，但js仍然是单线程语言
+    const worker = new Worker('./worker.js')
+  })
+
+const translate = () => {
+  setDisabled(true)
+  // 前端做AI 的缺点？  Transformer.js 浏览器运行大模型
+  // 浏览器运行大模型性能差
+  // 交给 web worker 交给独立线程机制
+}
+
     // ref reactive Model 
     // useState hooks 思想直接内置到框架
   const headerEle = (
@@ -42,6 +60,7 @@ function App() {
             rows={3} 
             readOnly/>
         </div>
+        <button disabled={disabled} onClick={translate}>Translate</button>
       </div>
     </>
   )
